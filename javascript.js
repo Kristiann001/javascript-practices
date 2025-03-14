f82f7732e4b62a48f7c47dee73dde12119867757
Kristian Koome <originalkristiann@gmail.com>
2025-03-14 17:16:51 +0300

    add javascript-practice

---
+++ javascript.js
@@ -0,0 +1,31 @@
+const adding = document.querySelector(".container1")
+
+const newGame = 
+//Create an element
+document.createElement("div")
+//Add a class to the created element
+newGame.classList.add("new_game")
+//Add text in the element class 
+newGame.textContent = "I am from Nairobi"
+//Styling the text in the element
+newGame.setAttribute("style", "color: red")
+
+//Add the text from the console to the HTMl code
+adding.appendChild(newGame)
+console.log(newGame)
+
+
+const oldGame = document.createElement("div")
+
+oldGame.classList.add("old_game")
+oldGame.textContent = "I was raised in Mombasa"
+
+adding.appendChild(oldGame)
+console.log(oldGame)
+
+
+const update = document.querySelector(".container2")
+
+update.setAttribute("class","new_container")
+
+console.log(update)
